e5a9bc36e55eb616e1e675d8432ab95745464ac7
BrunoAgst <bruno_agst@hotmail.com>
2020-01-20 23:21:46 -0300

    "correção classe customer"

--- src/models/konduto_customer.js
+++ src/models/konduto_customer.js
@@ -1,12 +1,12 @@
 class Customer{
-	constructor(id, name, tax_id, dob, phone1, phone2, email, created_at, New, vip){
+	constructor(id, name, email, tax_id, dob, phone1, phone2, created_at, New, vip){
 		this.id = id;
 		this.name = name;
+		this.email = email;
 		this.tax_id = tax_id;
 		this.dob = dob;
 		this.phone1 = phone1;
 		this.phone2 = phone2;
-		this.email = email;
 		this.created_at = created_at;
 		this.new = New;
 		this.vip = vip;
