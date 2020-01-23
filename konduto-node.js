c7cda5cc22a13cb1003cb724a42099e9aa55cf97
Bruno Augusto <bruno_agst@hotmail.com>
2020-01-23 20:09:32 +0000

    detalhando funções e criando constantes para url

--- konduto-node.js
+++ konduto-node.js
@@ -1,9 +1,17 @@
 const axios = require('axios');
+const url = "https://api.konduto.com/v1/orders";
+const urlBlacklist = "https://api.konduto.com/v1/blacklist/email"; 
 
-//GET
+/*
+    Consultar pedido na API da Konduto.
+
+    - order = número pedido.
+    
+    - secretkey = a chave privada de sua loja.
+*/
 
 module.exports.getOrder = function(order, secretkey){
-  axios.get(`https://api.konduto.com/v1/orders/${order}`,
+  axios.get(url + `/${order}`,
   { auth: { username: `${secretkey}`,  password: ''}})
   .then(function(response){
       return console.log(response.data);
@@ -13,13 +21,22 @@ module.exports.getOrder = function(order, secretkey){
   })
 }
 
-//PUT
+/*
+    Alterar status do pedido na API da Konduto.
+
+    - order = número do pedido.
+
+    - status = novo status do pedido (APPROVED, DECLINED, NOT_AUTHORIZED, CANCELED ou FRAUD).
+
+    - secretkey = a chave privada de sua loja.
+*/
+
 
 module.exports.putOrder = function(order, status ,secretkey){
 
   let statusJSON = JSON.stringify(status);
 
-  axios.put(`https://api.konduto.com/v1/orders/${order}`, statusJSON,
+  axios.put(url + `/${order}`, statusJSON,
   {  auth: { username: `${secretkey}`, password: ''}})
   .then(function(response){
       return console.log(response.data);
@@ -29,13 +46,20 @@ module.exports.putOrder = function(order, status ,secretkey){
   })
 }
 
-//POST
+/*
+    Enviar pedido para a API da Konduto.
+
+    - newOrder = novo pedido.
+
+    - secretkey = a chave privada de sua loja.
+
+*/
 
 module.exports.sendOrder = function(newOrder, secretkey){
 
   let orderJSON = JSON.stringify(newOrder);
 
-  axios.post(`https://api.konduto.com/v1/orders`, orderJSON,
+  axios.post(url, orderJSON,
   {  auth: { username: `${secretkey}`, password: ''}})
   .then(function(response){
       return console.log(response.data);
@@ -45,10 +69,16 @@ module.exports.sendOrder = function(newOrder, secretkey){
   })
 }
 
-//GET Blacklist
+/*
+    Consultar e-mail na Blacklist
+
+    - email = endereço do e-mail
+
+    - secretkey = a chave privada de sua loja
+*/
 
 module.exports.getBlacklist = function(email, secretkey){
-  axios.get(`https://api.konduto.com/v1/blacklist/email/${email}`,
+  axios.get(urlBlacklist + `/${email}`,
   {  auth: { username: `${secretkey}`, password: ''}})
   .then(function(response){
       return console.log(response.data);
@@ -58,10 +88,18 @@ module.exports.getBlacklist = function(email, secretkey){
   })
 }
 
-//PUT Blacklist
+/*
+    Alterar data de expiração do e-mail
+
+    - email = endereço do e-mail 
+
+    - days = quantidade de dias
+
+    - secretkey = a chave privada de sua loja
+*/
 
 module.exports.putBlacklist = function(email, secretkey, days){
-  axios.put(`https://api.konduto.com/v1/blacklist/email/${email}`,{"days_to_expire": days},
+  axios.put(urlBlacklist + `/${email}`,{"days_to_expire": days},
   {  auth: { username: `${secretkey}`, password: ''}})
   .then(function(response){
       return console.log(response.data);
@@ -71,13 +109,19 @@ module.exports.putBlacklist = function(email, secretkey, days){
   })
 }
 
-//POST Blacklist
+/*
+    Adicionar e-mail na blacklist
+
+    - email = endereço do e-mail 
+
+    - secretkey = a chave privada de sua loja
+*/
 
 module.exports.addBlacklist = function(email, secretkey){
 
   let emailJSON = JSON.stringify(email);
 
-  axios.post(`https://api.konduto.com/v1/blacklist/email`, emailJSON,
+  axios.post(urlBlacklist, emailJSON,
   {  auth: { username: `${secretkey}`, password: ''}})
   .then(function(response){
       return console.log(response.data);
@@ -87,11 +131,18 @@ module.exports.addBlacklist = function(email, secretkey){
   })
 }
 
-//DELETE Blacklist
+/*
+    Deletar e-mail blacklist
+
+    - email = endereço do e-mail 
+
+    - secretkey = a chave privada de sua loja
+
+*/
 
 module.exports.deleteBlacklist = function(email, secretkey){
 
-  axios.delete(`https://api.konduto.com/v1/blacklist/email/${email}`,
+  axios.delete(urlBlacklist + `/${email}`,
   {  auth: { username: `${secretkey}`, password: ''}})
   .then(function(response){
       return console.log(response.data);
